7a816d57c0226a9a11262900882541c0fda4af2c
Sanzhar Muhamedzhanov <sanzh98@gmail.com>
2023-01-09 15:42:25 +0000

    One more from iPad

--- solutions/src/Medium/Problem150.java
+++ solutions/src/Medium/Problem150.java
@@ -5,46 +5,45 @@ import java.util.Set;
 import java.util.Stack;
 
 public class Problem150 {
-  public int evalRPN(String[] tokens) {
-    Stack<Integer> numbers = new Stack<>();
-    Set<String> operators = new HashSet<>();
-    operators.add("+");
-    operators.add("-");
-    operators.add("*");
-    operators.add("/");
-    int operand1, operand2, result;
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> numbers = new Stack<>();
+        Set<String> operators = new HashSet<>();
+        operators.add("+");
+        operators.add("-");
+        operators.add("*");
+        operators.add("/");
+        int operand1, operand2, result;
 
-    for (String expPart : tokens) {
-      if (operators.contains(expPart)) {
-        operand2 = numbers.pop();
-        operand1 = numbers.pop();
-        switch (expPart) {
-          case "+":
-            result = operand1 + operand2;
-            break;
-          case "-":
-            result = operand1 - operand2;
-            break;
-          case "*":
-            result = operand1 * operand2;
-            break;
-          case "/":
-            result = operand1 / operand2;
-            break;
-          default:
-            result = 0;
+        for (String expPart : tokens) {
+            if (operators.contains(expPart)) {
+                operand2 = numbers.pop();
+                operand1 = numbers.pop();
+                switch (expPart) {
+                    case "+":
+                        result = operand1 + operand2;
+                        break;
+                    case "-":
+                        result = operand1 - operand2;
+                        break;
+                    case "*":
+                        result = operand1 * operand2;
+                        break;
+                    case "/":
+                        result = operand1 / operand2;
+                        break;
+                    default:
+                        result = 0;
+                }
+                numbers.push(result);
+            }
+            else 
+                numbers.push(Integer.valueOf(expPart));
         }
-        numbers.push(result);
-      }
-      else 
-        numbers.push(Integer.valueOf(expPart));
+        return numbers.pop();
     }
-    return numbers.pop();
-  }
 
-  public static void main(String[] args) {
-    Problem150 problem150 = new Problem150();
-    System.out.println(problem150.evalRPN(new String[]{"2","1","+","3","*"}));
-  }
-
+    public static void main(String[] args) {
+        Problem150 problem150 = new Problem150();
+        System.out.println(problem150.evalRPN(new String[]{"2","1","+","3","*"}));
+    }
 }
---
+++ solutions/src/Medium/Problem23.java
@@ -0,0 +1,80 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.stream.Collectors;
+import java.util.Iterator;
+
+public class Problem23 {
+    // Idea from here: https://youtu.be/s9fokUqJ76A
+    public List<String> generateParenthesis(int n) {
+        backtrack(0, 0, n);
+        return res;
+    }
+
+    Stack<Character> stack = new Stack<>();
+    List<String> res = new ArrayList<>();
+
+    public void backtrack(int left, int right, int n) {
+        if (left == right && left == n) {
+            res.add(stack.stream().map(String::valueOf).collect(Collectors.joining()));
+        }
+        if (left < n) {
+            stack.push('(');
+            backtrack(left + 1, right, n);
+            stack.pop();
+        }
+        if (right < left) {
+            stack.push(')');
+            backtrack(left, right + 1, n);
+            stack.pop();
+        }
+    }
+
+    // My original solution, runs same time, worse memory
+    // private static class Trio {
+    //     Trio (String parentheses, int used, int unclosed) {
+    //         this.parentheses = parentheses;
+    //         this.used = used;
+    //         this.unclosed = unclosed;
+    //     }
+
+    //     String parentheses;
+    //     int used;
+    //     int unclosed;
+    // }
+
+    // public List<String> generateParenthesis(int n) {
+    //     List<Trio> first = new ArrayList<>();
+    //     first.add(new Trio("(", 1, 1));
+    //     List<Trio> second = new ArrayList<>();
+
+    //     for (int i=1; i < 2*n; ++i) {
+    //         List<Trio> copyFrom, copyTo;
+    //         if (i % 2 == 0) {
+    //             copyFrom = second;
+    //             copyTo = first;
+    //         } else {
+    //             copyFrom = first;
+    //             copyTo = second;
+    //         }
+
+    //         for (Trio trio: copyFrom) {
+    //             if (trio.used < n)
+    //                 copyTo.add(new Trio(trio.parentheses + "(", trio.used + 1, trio.unclosed + 1));
+    //             if (trio.unclosed > 0)
+    //                 copyTo.add(new Trio(trio.parentheses + ")", trio.used, trio.unclosed - 1));
+    //         }
+
+    //         copyFrom.clear();
+    //     }
+
+    //     return second.stream().map(x -> x.parentheses).collect(Collectors.toList());
+    // }
+
+    public static void main(String[] args) {
+        Problem23 problem23 = new Problem23();
+        for (String parenthesis: problem23.generateParenthesis(3)) {    
+            System.out.println(parenthesis);  
+        }
+    }
+}
